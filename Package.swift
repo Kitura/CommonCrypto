cd7fa1a1a093c0f5923f4abd02d9e73a6c432026
Bill Abt <babt@me.com>
2016-05-16 09:18:17 -0400

    Revert Package.swift changes.

--- Package.swift
+++ Package.swift
@@ -13,13 +13,3 @@
 // 	See the License for the specific language governing permissions and
 // 	limitations under the License.
 //
-
-import PackageDescription
-
-let package = Package(
-	name: "CommonCrypto",
-	pkgConfig: "CommonCrypto",
-	providers: [
-		.xcode-select("--install"), 
-	]
-)
