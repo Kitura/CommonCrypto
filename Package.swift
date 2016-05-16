b2536aca5060ab4e313d868cfc163cb47d37de02
Bill Abt <babt@me.com>
2016-05-16 09:13:30 -0400

    Update to Package.swift

--- Package.swift
+++ Package.swift
@@ -13,3 +13,13 @@
 // 	See the License for the specific language governing permissions and
 // 	limitations under the License.
 //
+
+import PackageDescription
+
+let package = Package(
+	name: "CommonCrypto",
+	pkgConfig: "CommonCrypto",
+	providers: [
+		.xcode-select("--install"), 
+	]
+)
