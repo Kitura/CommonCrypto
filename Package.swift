570b293903ccbd3577b3eee0ec73ae2ee56443a0
Bouke Haarsma <bouke@haarsma.eu>
2016-07-08 13:08:09 +0200

    Empty manifest no longer supported by SwiftPM

--- Package.swift
+++ Package.swift
@@ -13,3 +13,9 @@
 // 	See the License for the specific language governing permissions and
 // 	limitations under the License.
 //
+
+import PackageDescription
+
+let package = Package(
+    name: "CommonCrypto"
+)
